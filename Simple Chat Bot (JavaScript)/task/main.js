56600d59a8516ebc16ace0d91a3cfcb33cb307ea
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-25 08:45:51 +0200

    Stage 3/5

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,8 +1,20 @@
 const input = require('sync-input');
+
 console.log("Hello! My name is Aid.");
-console.log("I was created in 2022.");
+console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-// use input() + to get the user's output
-let your_name = input();
-console.log(`What a great name you have, ${your_name}!`);
+let name = input();
+
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+
+// reading all remainders
+let remainder3 = input();
+let remainder5 = input();
+let remainder7 = input();
+
+let your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+console.log(`Your age is ${your_age}; that's a good time to start programming!`);
