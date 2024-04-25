f6e9d02bb27904638b203170d1ba938fcaae8d14
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-25 08:42:09 +0200

    Stage 2/5

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,2 +1,8 @@
-console.log("Hello! My name is Anna.");
+const input = require('sync-input');
+console.log("Hello! My name is Aid.");
 console.log("I was created in 2022.");
+console.log("Please, remind me your name.");
+
+// use input() + to get the user's output
+let your_name = input();
+console.log(`What a great name you have, ${your_name}!`);
